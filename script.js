f56493b801e7056d49f299c4b8eb9a174732f40b
VG FRAGRANCE <abuzarvevariya@gmail.com>
2026-08-01 23:27:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+window.addEventListener("load", function () {
+    console.log("VG FRAGRANCE Loaded");
 
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            card.style.transform = "scale(1.03)";
+            setTimeout(() => {
+                card.style.transform = "";
+            }, 200);
+        });
+    });
+});
